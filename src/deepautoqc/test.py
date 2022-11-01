e7eac41e929443877bebe32d7c92cdc206ee38b8
dominikgoeller <22domig@gmail.com>
2022-11-01 14:27:23 +0100

    created test.py pipeline to predict classification of user input svg file and returns prediction as json file in predictions folder

--- src/deepautoqc/test.py
+++ src/deepautoqc/test.py
@@ -1,23 +1,29 @@
 import io
+import json
+import sys
+from argparse import SUPPRESS, ArgumentParser
 from pathlib import Path
 
 import cairosvg
 import numpy as np
 import torch
+import torch.nn.functional as F
 from args import config
 from PIL import Image
 from torch import nn
 from torch.utils.data import DataLoader
 
-from deepautoqc.data import TestSkullstripDataset, generate_test_loader
-from deepautoqc.utils import (
+sys.path.append("..", 0)
+
+from data import TestSkullstripDataset, generate_test_loader  # noqa: E402
+from utils import (  # noqa: E402
     create_skullstrip_list,
     device_preparation,
     load_model,
 )
 
 
-def svgRead(filename):
+def svgRead(filename: str) -> np.ndarray:
     """Load an SVG file and return image in Numpy array"""
     # Convert SVG to PNG in memory
     png_content = cairosvg.svg2png(url=filename, output_height=870, output_width=2047)
@@ -29,6 +35,12 @@ def svgRead(filename):
     return image_without_alpha
 
 
+def image_to_tensor(image: np.ndarray) -> np.ndarray:
+    image = image.transpose((2, 0, 1))
+    image = torch.from_numpy(image).float() / 255
+    return image
+
+
 def test(model, test_l: DataLoader, device: torch.device):
     with torch.no_grad():
         for images in test_l:
@@ -38,23 +50,86 @@ def test(model, test_l: DataLoader, device: torch.device):
             _, preds = torch.max(outputs.data, 1)
 
 
-def main(test_data_path: Path, model_ckpt: Path):
-    test_skullstrips = create_skullstrip_list(usable_dir=test_data_path)
-    dataset = TestSkullstripDataset(skullstrips=test_skullstrips)
+def predict(model, svg_path: str):
+    if torch.cuda.is_available():
+        model.cuda()
+    image = svgRead(filename=svg_path)
+    tensor = image_to_tensor(image=image).unsqueeze(
+        0
+    )  # add 4th dimension as is expected input
+    with torch.no_grad():
+        output = model(tensor)
+        _, preds = torch.max(output.data, 1)
+        x = F.softmax(output, dim=1).detach().cpu().numpy()  # or output[0], dim=0
+    x = np.round(x, 4)
+    preds = preds.cpu().numpy()
+    # usable = 1, unusable = 0
 
-    test_loader = generate_test_loader(
-        dataset=dataset, batchsize=config.batch_size, num_workers=config.num_workers
-    )
+    if preds[0] == 1:
+        classification = "usable"
+        prob = x[0][1]
+    elif preds[0] == 0:
+        classification = "unusable"
+        prob = x[0][0]
+    prob = str(np.round(100 * prob, 2)) + "%"
+    sub_name = svg_path.split("/")[
+        -1
+    ]  # expects input to be */*/../sub-102008_skull_strip_report.svg
+    pred_dict = {
+        "sub_name": sub_name,
+        "prediction": classification,
+        "probability": prob,
+    }
+    json_obj = json.dumps(pred_dict, indent=4)
+
+    save_path = "./predictions/" + sub_name.split(sep=".")[0] + ".json"
+    with open(save_path, "w") as outputfile:
+        outputfile.write(json_obj)
+    print(f".. Finished prediction check {save_path} for results!! ..")
+    # print(f"Output of model: {output}")
+    # print(f"Prediction: {preds[0]}")
+    # print(f"Softmax: {x}")
+    # print(prob)
+    # print(classification)
+
+
+def main(model_ckpt: Path, svg_path: str):  # test_data_path: Path,
+    # test_skullstrips = create_skullstrip_list(usable_dir=test_data_path)
+    # dataset = TestSkullstripDataset(skullstrips=test_skullstrips)
+
+    # test_loader = generate_test_loader(
+    #    dataset=dataset, batchsize=config.batch_size, num_workers=config.num_workers
+    # )
 
     model = load_model(model_filepath=model_ckpt)
-
-    device, device_ids = device_preparation(n_gpus=config.n_gpus)
-    model.to(device=device)
-    if len(device_ids) > 1:
-        model = nn.DataParallel(module=model, device_ids=device_ids)
-    model.to(device=device)
-    test(model=model, test_l=test_loader, device=device)
+    print(".. Finished loading model! ..")
+    # device, device_ids = device_preparation(n_gpus=config.n_gpus)
+    # model.to(device=device)
+    # if len(device_ids) > 1:
+    #    model = nn.DataParallel(module=model, device_ids=device_ids)
+    # model.to(device=device)
+    # test(model=model, test_l=test_loader, device=device)
+    predict(model=model, svg_path=svg_path)
 
 
 if __name__ == "__main__":
-    main(test_data_path=config.TEST_DATA_PATH, model_ckpt=config.DUMMY_MODEL_CKPT)
+    parser = ArgumentParser(
+        description="Input the path of your SVG file for prediction!", add_help=False
+    )
+    required = parser.add_argument_group("required arguments")
+    optional = parser.add_argument_group("optional arguments")
+
+    # Add back help
+    optional.add_argument(
+        "-h",
+        "--help",
+        action="help",
+        default=SUPPRESS,
+        help="show this help message and exit",
+    )
+    required.add_argument("-i", "--input", help="Input path to svg file", required=True)
+    args = parser.parse_args()
+    svg_path = args.input
+    main(model_ckpt=config.DUMMY_MODEL_CKPT, svg_path=svg_path)
+
+    # main(test_data_path=config.TEST_DATA_PATH, model_ckpt=config.DUMMY_MODEL_CKPT)
