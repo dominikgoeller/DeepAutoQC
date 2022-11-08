f12bdc3ec9392fd2becbff06b7e25c7f456ac736
dominikgoeller <22domig@gmail.com>
2022-11-08 12:55:45 +0100

    modified args/paths

--- src/deepautoqc/args.py
+++ src/deepautoqc/args.py
@@ -23,14 +23,12 @@ class config:
     n_gpus = 4
     batch_size = 16
     epochs = 30
-    num_workers = (
-        32  # checked in jupyter notebook for optimal value. 2 epochs in 229 secs
-    )
+    num_workers = 32  # checked in jupyter notebook for optimal value. 2 training (w/o validation) epochs in 229 secs
     lr = 1e-3
     momentum = 0.9
     optimizer = "SGD"  # or "ADAM"
     """
-    Folders for checkpoints
+    Paths for checkpoints
     """
     EARLYSTOP_PATH = "/data/gpfs-1/users/goellerd_c/work/saved_models/ResNet50trueSGD.pt"  # path to save model on hpc-cluster
-    DUMMY_MODEL_CKPT = "./results/ResNet50/EarlyStopcheckpoint2510.pt"  # src/deepautoqc/results/ResNet50/EarlyStopcheckpoint2510.pt
+    MODEL_CKPT = "/Users/dominik/Downloads/ResNet50trueADAM.pt"  # ./results/ResNet50/EarlyStopcheckpoint2510.pt"  # src/deepautoqc/results/ResNet50/EarlyStopcheckpoint2510.pt
