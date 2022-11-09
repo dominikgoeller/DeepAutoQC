36d136d294f700b9380d6b28441f42af5ea30d35
dominikgoeller <22domig@gmail.com>
2022-11-09 23:17:53 +0100

    confusion matrix and default losses func

--- src/deepautoqc/metrics.py
+++ src/deepautoqc/metrics.py
@@ -7,7 +7,21 @@ def confusion_matrix(actual, predicted):
     assert len(actual) == len(predicted)
     confusion_matrix = metrics.confusion_matrix(y_true=actual, y_pred=predicted)
     cm_display = metrics.ConfusionMatrixDisplay(
-        confusion_matrix=confusion_matrix, display_labels=[False, True]
+        confusion_matrix=confusion_matrix, display_labels=["unusable", "usable"]
     )
     cm_display.plot()
     plt.show()
+
+
+def plot_losses():
+    return None
+
+
+def main():
+    actual = [0, 1, 0, 1, 1]
+    pred = [1, 0, 1, 0, 1]
+    confusion_matrix(actual=actual, predicted=pred)
+
+
+if __name__ == "__main__":
+    main()
