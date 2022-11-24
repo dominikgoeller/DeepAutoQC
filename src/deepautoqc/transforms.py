da04d0fa366fb3c61378ab3ce6c32b7d030c2501
dominikgoeller <22domig@gmail.com>
2022-11-24 19:56:52 +0100

    reworked the transforms pipeline

--- src/deepautoqc/transforms.py
+++ src/deepautoqc/transforms.py
@@ -12,78 +12,45 @@ class BaseBrain(ABC):
         self.t1w = transform_data().get_base_transform()(t1w)
         self.mask = transform_data().get_base_transform()(mask)
 
+    # @abstractmethod
+    # def compose():
+    #    pass
     @abstractmethod
-    def compose():
+    def apply():
         pass
 
 
 class badBrain(BaseBrain):
     def __init__(self, t1w: nib.Nifti1Image, mask: nib.Nifti1Image):
         super().__init__(t1w, mask)
+        self.t1w = t1w  # we want to have the t1w with the applied changes from super()
+        self.mask = (
+            mask  # we want to have the mask with the applied changes from super()
+        )
 
-    def compose():
-        """Here we want to apply bad transformations to our t1w or mask or both
-        we differentiate between scanner artefacts and articial artefacts that only transform to our mask aka red outline
-        We want to have many different composed transforms available so we can randomly choose one of them with tio.OneOf() for our data augmentation inside the
-        dataset for the training process.
-        """
-        pass
+    # def compose(self):
+    #    """Here we want to apply bad transformations to our t1w or mask or both
+    #    we differentiate between scanner artefacts and articial artefacts that only transform to our mask aka red outline
+    #    We want to have many different composed transforms available so we can randomly choose one of them with tio.OneOf() for our data augmentation inside the
+    #    dataset for the training process.
+    #    """
+    #    return transform_data().choose_bad_transform()
+    def apply(self):
+        transf = transform_data().choose_bad_transform()
+        t1w = transf(self.t1w)
+        mask = transf(self.mask)
+        return t1w, mask
 
 
 class goodBrain(BaseBrain):
     def __init__(self, t1w: nib.Nifti1Image, mask: nib.Nifti1Image):
         super().__init__(t1w, mask)
 
-    def compose():
+    def compose(self):
         """See above for documentation at compose_bad()"""
         pass
 
 
-# class transforms_cfg:
-#    t1_real = tio.Compose(
-#        [
-#            tio.RandomMotion(degrees=20, translation=20, num_transforms=3),
-#            tio.RandomGhosting(
-#                num_ghosts=(4, 10),
-#                axes=("AP", "lr"),
-#                intensity=(0.1, 0.3),
-#                restore=0.05,
-#            ),
-#            tio.RescaleIntensity(out_min_max=(0, 1)),
-#        ]
-#    )
-#    t2_real = tio.Compose(
-#        [
-#            tio.RandomMotion(degrees=20, translation=20, num_transforms=3),
-#            tio.RandomGhosting(
-#                num_ghosts=(4, 10),
-#                axes=("AP", "lr"),
-#                intensity=(0.1, 0.3),
-#                restore=0.05,
-#            ),
-#            tio.RandomSpike(num_spikes=(3), intensity=(1, 3)),
-#            tio.RescaleIntensity(out_min_max=(0, 1)),
-#        ]
-#    )
-#
-#    real_artifacts = {
-#        t1_real,
-#        t2_real,
-#    }  # for further use in data.py tio.OneOf(real_artifacts)
-#
-#    # artificial artifacts for red outline
-#    t1_ai = tio.Compose(
-#        [
-#            tio.RandomAffine(degrees=(20), center="image"),
-#            tio.RescaleIntensity(out_min_max=(0, 1)),
-#        ]
-#    )
-#
-#    # wait with further transforms until meeting
-#    ai_artifacts = {t1_ai}
-
-
-## functions nach diesem Muster definieren?
 @dataclass
 class transform_data:
     motion: dict = field(
@@ -119,6 +86,12 @@ class transform_data:
             "num_control_points": 7,
             "max_displacement": (7, 9, 5),
             "locked_borders": 2,
+        }
+    )
+    swap: dict = field(
+        default_factory=lambda: {
+            "patch_size": 15,
+            "num_iterations": 40,
         }
     )
 
@@ -161,6 +134,12 @@ class transform_data:
             locked_borders=self.elastic.get("locked_borders"),
         )
 
+    def get_swap(self):
+        return tio.RandomSwap(
+            patch_size=self.swap.get("patch_size"),
+            num_iterations=self.swap.get("num_iterations"),
+        )
+
     def get_base_transform(self):
         return tio.Compose(
             [
@@ -168,6 +147,60 @@ class transform_data:
                 self.get_elastic(),
             ]
         )
+
+    def bad_scanner_dict(self):
+        """Dictionary with instances of
+        :class:`~torchio.transforms.Transform` as keys and
+        probabilities as values. Probabilities are normalized so they sum
+        to one. If a sequence is given, the same probability will be
+        assigned to each transform."""
+        bad_scanner_dict: dict = {
+            tio.Compose(
+                [
+                    self.get_motion(),
+                    self.get_ghosting(),
+                ]
+            ),
+            tio.Compose(
+                [
+                    self.get_motion(),
+                    self.get_ghosting(),
+                    self.get_spike(),
+                ]
+            ),
+            tio.Compose(
+                [
+                    self.get_motion(),
+                    self.get_ghosting(),
+                    self.get_spike(),
+                    self.get_swap(),
+                ]
+            ),
+        }
+        return bad_scanner_dict
+
+    def bad_synthetic_dict(self):
+        """Dictionary with instances of
+        :class:`~torchio.transforms.Transform` as keys and
+        probabilities as values. Probabilities are normalized so they sum
+        to one. If a sequence is given, the same probability will be
+        assigned to each transform."""
+        bad_synthetic_dict: dict = {
+            tio.Compose([self.get_affine()]),
+        }
+        return bad_synthetic_dict
+
+    def choose_bad_transform(self):
+        bad_transform = tio.Compose(
+            [
+                tio.OneOf(
+                    tio.OneOf(self.bad_scanner_dict()),
+                    tio.OneOf(self.bad_synthetic_dict()),
+                ),  # PROBLEM! we want to apply synthetic transforms only on mask and not t1w which is currently not the case!
+                self.get_rescale(),
+            ]
+        )
+        return bad_transform
 
 
 # def motion(cfg=trf_cfg()):
