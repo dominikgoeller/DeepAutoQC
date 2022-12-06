de3066036f40d3fe3c274646aee9e9f00c271898
dominikgoeller <22domig@gmail.com>
2022-12-06 15:03:52 +0100

    added model parameters count

--- src/deepautoqc/models.py
+++ src/deepautoqc/models.py
@@ -4,7 +4,7 @@ import torch
 from args import config
 from torch import nn
 from torchvision import models
-from torchvision.models import ResNet50_Weights
+from torchvision.models import ResNet
 
 DEFAULT_WEIGHTS = Path(
     "/data/gpfs-1/users/goellerd_c/work/git_repos/DeepAutoQC/src/deepautoqc/weights/resnet50-DEFAULT.pth"
@@ -14,6 +14,10 @@ cwd = Path.cwd()
 mod_path = Path(__file__).parent
 relative_path = "weights/resnet50-DEFAULT.pth"
 weight_path = (mod_path / relative_path).resolve()
+
+
+def count_parameters(model_conv: ResNet):
+    return sum(p.numel() for p in model_conv.parameters() if p.requires_grad)
 
 
 def resnet50(requires_grad: bool = config.requires_grad, weights_path=weight_path):
@@ -35,4 +39,6 @@ def resnet50(requires_grad: bool = config.requires_grad, weights_path=weight_pat
     num_ftrs = model.fc.in_features
     # model.avgpool is already an adaptivepoolinglayer which handles input tensors of all sizes and adapts its pooling to the specified output dims
     model.fc = nn.Linear(num_ftrs, config.num_classes)
+    print("...Finished loading model...")
+    print(f"The model has {count_parameters(model):,} trainable parameters")
     return model
