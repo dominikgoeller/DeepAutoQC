1c4c6138322f33f6da15e9bcb6734958b187c2c4
dominikgoeller <22domig@gmail.com>
2022-12-10 14:55:26 +0100

    args

--- src/deepautoqc/args.py
+++ src/deepautoqc/args.py
@@ -32,4 +32,4 @@ class config:
     Paths for checkpoints
     """
     EARLYSTOP_PATH = "./ckpts/ResNet50/"  # path to save model
-    MODEL_CKPT = "/Users/dominik/Downloads/ResNet50trueADAM.pt"  # Path to checkpoint to resume training or predict./results/ResNet50/EarlyStopcheckpoint2510.pt"  # src/deepautoqc/results/ResNet50/EarlyStopcheckpoint2510.pt
+    MODEL_CKPT = "/data/gpfs-1/users/goellerd_c/work/git_repos/DeepAutoQC/src/deepautoqc/ckpts/ResNet50/2022-12-05/ResNet_trainable_Adam.pt"  # Path to checkpoint to resume training or predict./results/ResNet50/EarlyStopcheckpoint2510.pt"  # src/deepautoqc/results/ResNet50/EarlyStopcheckpoint2510.pt
