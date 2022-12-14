836c544a5e087e502669a14ff84208d6549f7b85
dominikgoeller <22domig@gmail.com>
2022-12-14 11:21:12 +0100

    predicting BIDS folder implemented

--- src/deepautoqc/test.py
+++ src/deepautoqc/test.py
@@ -10,6 +10,7 @@ import torch
 import torch.nn.functional as F
 from args import config
 from data import TestSkullstripDataset, generate_test_loader  # noqa: E402
+from halfpipe.file_index.bids import BIDSIndex
 from PIL import Image
 from torch import nn
 from torch.utils.data import DataLoader
@@ -38,46 +39,69 @@ def image_to_tensor(image: np.ndarray) -> np.ndarray:
     return image
 
 
-def test(model, test_l: DataLoader, device: torch.device):
-    with torch.no_grad():
-        for images in test_l:
-            images = images.to(device, non_blocking=True)
+def predict_reports(model, folder_path: str):
+    save_path = "./predictions/" + "exclude" + ".json"
+    results = list()
 
-            outputs = model(images)
-            _, preds = torch.max(outputs.data, 1)
+    for path in Path(folder_path).rglob("*strip_report.svg"):
+        pred_dict = predict(model=model, svg_path=str(path))
+        results.append(pred_dict)
+
+    with open(save_path, "w") as outputfile:
+        json_obj = json.dumps(results, indent=4)
+        outputfile.write(json_obj)
+
+    print(f".. Finished prediction check {save_path} for results!! ..")
 
 
 def predict(model, svg_path: str):
-    if torch.cuda.is_available():
-        model.cuda()
     image = svgRead(filename=svg_path)
     tensor = image_to_tensor(image=image).unsqueeze(
         0
     )  # add 4th dimension as is expected input
     with torch.no_grad():
         output = model(tensor)
-        _, preds = torch.max(output.data, 1)
-        x = F.softmax(output, dim=1).detach().cpu().numpy()  # or output[0], dim=0
+        # _, preds = torch.max(output, dim=1)
+        preds = torch.argmax(output)  # class
+        x = (
+            F.softmax(output, dim=1).detach().cpu().numpy()
+        )  # or output[0], dim=0 probability for class
+        # x = F.softmax(output, dim=1)
+        # conf, classes = torch.max(x, 1)
     x = np.round(x, 4)
     preds = preds.cpu().numpy()
-    # usable = 1, unusable = 0
 
-    if preds[0] == 1:
-        classification = "usable"
+    # usable = 1, unusable = 0
+    # print("Output", output)
+    # print("output data:", output.data)
+    # print("PREDS:", preds)
+    # print("_:", _)
+    print("x:", x)
+    if preds == 1:
+        classification = "good"
         prob = x[0][1]
-    elif preds[0] == 0:
-        classification = "unusable"
+    elif preds == 0:
+        classification = "bad"
         prob = x[0][0]
     # prob = str(np.round(100 * prob, 2)) + "%"  # as float, ohne 100 multiplikation
     prob = np.round(prob, 4)
     sub_name = svg_path.split("/")[
         -1
     ]  # expects input to be */*/../sub-102008_skull_strip_report.svg
+    number = sub_name.split("-")[-1].split("_")[0]
+    category = "skull_strip_report"
+    # pred_dict = {
+    #    "sub_name": sub_name,
+    #    "prediction": classification,
+    #    "probability": float(prob),
+    # }
     pred_dict = {
-        "sub_name": sub_name,
-        "prediction": classification,
-        "probability": prob,
+        "sub_name": number,
+        "type": category,
+        "rating": classification,
+        "confidence": float(prob),
     }
+    return pred_dict
     json_obj = json.dumps(pred_dict, indent=4)
 
     save_path = "./predictions/" + sub_name.split(sep=".")[0] + ".json"
@@ -94,8 +118,8 @@ def predict(model, svg_path: str):
 def main(model_ckpt: Path, svg_path: str):
 
     model = load_model(model_filepath=model_ckpt)
-    print(".. Finished loading model! ..")
-    predict(model=model, svg_path=svg_path)
+    # predict(model=model, svg_path=svg_path)
+    predict_reports(model=model, folder_path=svg_path)
 
 
 if __name__ == "__main__":
