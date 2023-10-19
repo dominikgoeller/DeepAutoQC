fe0b152b01661a2955a80cc392d7fcc563ddb345
Dominik <Dominik@mindbrains-MacBook-Pro.local>
2023-10-19 13:35:58 +0200

    added zstandard compression

--- src/deepautoqc/scripts/unpack.py
+++ src/deepautoqc/scripts/unpack.py
@@ -1,34 +1,56 @@
+import os
+import pickle
 from concurrent.futures import ProcessPoolExecutor
 from pathlib import Path
 from typing import List
+
+import zstandard as zstd
 
 from deepautoqc.data_structures import BrainScan
 from deepautoqc.utils import load_from_pickle, save_to_pickle
 
 
-def unpack_single_pickle(p, unpacked_dir):
+def compress_and_save(brain_scan, output_dir, compressor):
+    compressed_data = compressor.compress(pickle.dumps(brain_scan))
+    with open(
+        os.path.join(output_dir, f"{brain_scan.id}.pkl.zst"), "wb"
+    ) as compressed_file:
+        compressed_file.write(compressed_data)
+
+
+def unpack_single_pickle(p, unpacked_dir, compressed_dir):
     datapoints: List[BrainScan] = load_from_pickle(p)
+    compressor = zstd.ZstdCompressor()
     for brain_scan in datapoints:
+        # Compress and save the data
+        compress_and_save(brain_scan, compressed_dir, compressor)
+
+        # Save the original (uncompressed) data for reference if needed
         new_file_path = unpacked_dir / f"{brain_scan.id}.pkl"
         save_to_pickle(brain_scan, new_file_path)
 
 
-def unpack_pickles(path):
+def unpack_pickles(path, output_dir, compressed_dir):
     pickle_paths = list(Path(path).glob("*.pkl"))
     print(f"Number of pickle files: {len(pickle_paths)}")
 
-    # Create a directory for unpacked BrainScan objects
-    unpacked_dir = Path(
-        "/data/gpfs-1/users/goellerd_c/work/deep-auto-qc/parsed_dataset/skull_strip_report/original_unpacked"
-    )
+    unpacked_dir = Path(output_dir)
+    compressed_dir = Path(compressed_dir)
     unpacked_dir.mkdir(exist_ok=True)
+    compressed_dir.mkdir(exist_ok=True)
 
     with ProcessPoolExecutor() as executor:
         executor.map(
-            unpack_single_pickle, pickle_paths, [unpacked_dir] * len(pickle_paths)
+            unpack_single_pickle,
+            pickle_paths,
+            [unpacked_dir] * len(pickle_paths),
+            [compressed_dir] * len(pickle_paths),
         )
 
 
-unpack_pickles(
-    "/data/gpfs-1/users/goellerd_c/work/deep-auto-qc/parsed_dataset/skull_strip_report/original"
-)
+if __name__ == "__main__":
+    input_dir = "/data/gpfs-1/users/goellerd_c/work/deep-auto-qc/parsed_dataset/skull_strip_report/original"
+    output_dir = "/data/gpfs-1/users/goellerd_c/work/deep-auto-qc/parsed_dataset/skull_strip_report/original_unpacked"
+    compressed_dir = "/data/gpfs-1/users/goellerd_c/work/deep-auto-qc/parsed_dataset/skull_strip_report/original_unpacked_compressed"
+
+    unpack_pickles(input_dir, output_dir, compressed_dir)
