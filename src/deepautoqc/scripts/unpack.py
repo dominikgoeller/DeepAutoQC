29ba81dc656a43a2a693a0aec1ef4290d710b800
Dominik <Dominik@mindbrains-MacBook-Pro.local>
2023-11-02 13:42:45 +0100

    path changed

--- src/deepautoqc/scripts/unpack.py
+++ src/deepautoqc/scripts/unpack.py
@@ -24,7 +24,9 @@ def unpack_single_pickle(p):
     #    "/data/gpfs-1/users/goellerd_c/scratch/deep-auto-qc/parsed_dataset/skull_strip_report/original_unpacked"
     # )
     # compressed_dir = "/data/gpfs-1/users/goellerd_c/scratch/deep-auto-qc/parsed_dataset/skull_strip_report/original_unpacked_compressed"
-    compressed_dir = "/data/gpfs-1/users/goellerd_c/scratch/deep-auto-qc/parsed_dataset/skull_strip_report/original_unpacked_bad_compressed"
+    compressed_dir = Path(
+        "/data/gpfs-1/users/goellerd_c/scratch/deep-auto-qc/parsed_dataset/skull_strip_report/original_unpacked_bad_compressed"
+    )
     compressed_dir.mkdir(exist_ok=True)
     # datapoints: List[BrainScan] = load_from_pickle(p)
     datapoints: List[BrainScan] = p
